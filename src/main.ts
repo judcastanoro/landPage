df8dc5571ec12439f85188d5f0d56cec843551f3
JDCR <judcastanoro@unal.edu.co>
2024-01-23 10:57:28 -0500

    Pruebas 5

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,29 @@
  */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { Component } from '@angular/core';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>Hello from {{ name }}!</h1>
+    <a target="_blank" href="https://angular.dev/overview">
+      Learn more about Angular
+    </a>
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

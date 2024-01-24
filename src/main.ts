03e50477801cd639cc1598dcf216dd47a3c23cea
JDCR <judcastanoro@unal.edu.co>
2024-01-23 20:01:41 -0500

    Prueba 7

--- src/main.ts
+++ src/main.ts
@@ -14,16 +14,16 @@ if (environment.production) {
 }
 
 @Component({
-  selector: 'app-root',
+  selector: 'ngx-app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
+    <h1>Hello from the otter side</h1>
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
   `,
 })
-export class App {
+export class AppComponent {
   name = 'Angular';
 }
 

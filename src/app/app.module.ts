7ba346d0cc083ec21479c0c1074f89f34d9999f7
JDCR <judcastanoro@unal.edu.co>
2024-01-26 17:37:52 -0500

    Prueba 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
 import {
   NbChatModule,
   NbDatepickerModule,
@@ -39,6 +40,7 @@ import {
     }),
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
+    PagesModule,
   ],
   bootstrap: [AppComponent],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,32 @@
  */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Component } from '@angular/core';
+import { Component,NgModule } from '@angular/core';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { RouterModule } from '@angular/router';
+import { PagesModule } from './app/pages/pages.module';
+import { LandpageModule } from './app/pages/landpage/landpage.module';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
+@NgModule({
+  imports: [
+    PagesModule,
+    AppModule,
+    RouterModule.forRoot([
+      { path: '', component: LandpageModule },
+    ]),
+  ],
+  declarations: [
+  ],
+  bootstrap: [AppComponent],
+})
+
+/*
 @Component({
   selector: 'ngx-app-root',
   standalone: true,
@@ -22,7 +40,7 @@ if (environment.production) {
       Learn more about Angular
     </a>
   `,
-})
+})*/
 export class AppComponent {
   name = 'Angular';
  }
